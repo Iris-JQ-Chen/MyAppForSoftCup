c9364f5052d152565577fb9b58d0ae5e95588ff4
Iris JQ Chen <2906514421@qq.com>
2019-06-15 18:39:22 +0800

    ShareSDK第三方登录，目前只能完成qq登录，微信和新浪需要开发者签名。

--- app/src/main/java/com/example/myappforsortcup/activity/RegisterActivity.java
+++ app/src/main/java/com/example/myappforsortcup/activity/RegisterActivity.java
@@ -18,21 +18,32 @@ import android.view.animation.BounceInterpolator;
 import android.view.animation.RotateAnimation;
 import android.view.animation.ScaleAnimation;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.example.myappforsortcup.R;
 import com.example.myappforsortcup.animationTest.LoginActivity;
 import com.example.myappforsortcup.util.SomeUtil;
 import com.jaouan.revealator.Revealator;
 import com.jaouan.revealator.animations.AnimationListenerAdapter;
+import com.mob.MobSDK;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import cn.sharesdk.framework.Platform;
+import cn.sharesdk.framework.PlatformActionListener;
+import cn.sharesdk.framework.PlatformDb;
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.sina.weibo.SinaWeibo;
+import cn.sharesdk.tencent.qq.QQ;
+import cn.sharesdk.wechat.friends.Wechat;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity implements PlatformActionListener {
 
     @BindView(R.id.fab) View mFab;
     @BindView(R.id.plane) View mPlaneImageView;
@@ -45,10 +56,14 @@ public class RegisterActivity extends AppCompatActivity {
     @BindView(R.id.input_name_on_register) EditText inputName;
     @BindView(R.id.input_password_on_register)EditText inputPassword;
     @BindView(R.id.input_email_on_register)EditText inputEmail;
+    @BindView(R.id.qq_on_register) ImageButton qqRegisterBtn;
+    @BindView(R.id.weixin_on_register) ImageButton weixinRegisterBtn;
+    @BindView(R.id.weibo_on_register) ImageButton weiboRegisterBtn;
 //    @BindView(R.id.login_on_register)TextView btnLogin;
 //    @BindView(R.id.register_on_register)LinearLayout btnRegister;
 
     private Intent intent = null;
+    private PlatformDb platDB; //平台授权数据DB
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,6 +71,7 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
         ButterKnife.bind(this);
 
+        MobSDK.init(this);
         initUserInfo();
         intent = new Intent(RegisterActivity.this,MainActivity.class);
         setupWindowAnimations();
@@ -70,25 +86,6 @@ public class RegisterActivity extends AppCompatActivity {
 
     @OnClick(R.id.register_on_register)
     void register(){
-//        String name = inputName.getText().toString();
-//        String password = inputPassword.getText().toString();
-//        String email = inputEmail.getText().toString();
-//        List<String> list = new ArrayList<>();
-//        list.add(name);
-//        list.add(password);
-//        list.add(email);
-//
-//        if (name.equals("") && password.equals("")){
-//            SomeUtil.shakeControl(inputName);
-//            SomeUtil.shakeControl(inputPassword);
-//        }else if (name.equals("") && !password.equals("")){
-//            SomeUtil.shakeControl(inputName);
-//        }else if (!name.equals("") && password.equals("")){
-//            SomeUtil.shakeControl(inputPassword);
-//        }else {
-//            //网络
-//            send();
-//        }
         boolean isEmpty = false;
         List<EditText> list = new ArrayList<>();
         list.add(inputName);
@@ -139,6 +136,78 @@ public class RegisterActivity extends AppCompatActivity {
             }
         });
         circularReveal.start();
+
+    }
+
+    @OnClick(R.id.qq_on_register)
+    void qqRegister(){
+//        Platform plat = ShareSDK.getPlatform(QQ.NAME);
+//        plat.removeAccount(true); //移除授权状态和本地缓存，下次授权会重新授权
+//        plat.SSOSetting(false); //SSO授权，传false默认是客户端授权，没有客户端授权或者不支持客户端授权会跳web授权
+//        plat.setPlatformActionListener(this);//授权回调监听，监听oncomplete，onerror，oncancel三种状态
+//        if(!plat.isClientValid()){
+//            //判断是否存在授权凭条的客户端，true是有客户端，false是无
+//            Toast.makeText(RegisterActivity.this,"尚未安装QQ客户端",Toast.LENGTH_SHORT).show();
+//        }
+//        if(plat.isAuthValid()){
+//            //判断是否已经存在授权状态，可以根据自己的登录逻辑设置
+//            Toast.makeText(this, "已经授权过了", Toast.LENGTH_SHORT).show();
+//            return;
+//        }
+//        ShareSDK.setActivity(this);//抖音登录适配安卓9.0
+//        plat.showUser(null);    //要数据不要功能，主要体现在不会重复出现授权界面
+        Platform qq = ShareSDK.getPlatform(QQ.NAME);
+        authorize(qq);
+    }
+
+    @OnClick(R.id.weixin_on_register)
+    void weixinRegister(){
+        Platform wechat = ShareSDK.getPlatform(Wechat.NAME);
+        authorize(wechat);
+    }
+
+    @OnClick(R.id.weibo_on_register)
+    void weiboRegister(){
+        Platform weibo = ShareSDK.getPlatform(SinaWeibo.NAME);
+        authorize(weibo);
+    }
+
+    //第三方授权登录
+    private void authorize(Platform plat) {
+
+        if (plat == null) {
+            return;
+        }
+
+
+        // true不使用SSO授权，false使用SSO授权
+        plat.SSOSetting(false);
+        plat.setPlatformActionListener(this);
+        plat.authorize();
+        //获取用户资料
+        plat.showUser(null);
+
+        //判断指定平台是否已经完成授权
+        if(plat.isAuthValid()) {
+            String token = plat.getDb().getToken();
+            String userId = plat.getDb().getUserId();
+            String name = plat.getDb().getUserName();
+            String gender = plat.getDb().getUserGender();
+            String headImageUrl = plat.getDb().getUserIcon();
+            String platformNname = plat.getDb().getPlatformNname();
+            if (userId != null) {
+
+                //已经授权过，直接下一步操作
+                if(platformNname.equals(SinaWeibo.NAME)) {
+                    //微博授权
+                } else if(platformNname.equals(QQ.NAME)) {
+                    //QQ授权
+                } else if(platformNname.equals(Wechat.NAME)) {
+                    //微信授权
+                }
+                return;
+            }
+        }
 
     }
 
@@ -205,4 +274,75 @@ public class RegisterActivity extends AppCompatActivity {
         getWindow().setEnterTransition(fade);
     }
 
+    @Override
+    public void onComplete(Platform platform, int i, HashMap<String, Object> hashMap) {
+        Toast.makeText(RegisterActivity.this,"onComplete",Toast.LENGTH_SHORT).show();
+        String headImageUrl = null;//头像
+        String userId;//userId
+        String token;//token
+        String gender;//性别
+        String name = null;//用户名
+
+        if (i == Platform.ACTION_USER_INFOR) {
+            Toast.makeText(RegisterActivity.this,"ACTION_USER_INFOR",Toast.LENGTH_SHORT).show();
+
+            platDB = platform.getDb(); // 获取平台数据DB
+
+            if (platform.getName().equals(Wechat.NAME)) {
+                //微信登录
+
+                // 通过DB获取各种数据
+                token = platDB.getToken();
+                userId = platDB.getUserId();
+                name = platDB.getUserName();
+                gender = platDB.getUserGender();
+                headImageUrl = platDB.getUserIcon();
+                if ("m".equals(gender)) {
+                    gender = "1";
+                } else {
+                    gender = "2";
+                }
+
+                Toast.makeText(RegisterActivity.this,token+"\n"+userId+"\n"+name+"\n"+gender+"\n"+headImageUrl,Toast.LENGTH_SHORT).show();
+
+            } else if (platform.getName().equals(SinaWeibo.NAME)) {
+                // 微博登录
+
+                token = platDB.getToken();
+                userId = platDB.getUserId();
+                name = hashMap.get("nickname").toString(); // 名字
+                gender = hashMap.get("gender").toString(); // 年龄
+                headImageUrl = hashMap.get("figureurl_qq_2").toString(); // 头像figureurl_qq_2 中等图，figureurl_qq_1缩略图
+                Toast.makeText(RegisterActivity.this,token+"\n"+userId+"\n"+name+"\n"+gender+"\n"+headImageUrl,Toast.LENGTH_SHORT).show();
+
+            } else if (platform.getName().equals(QQ.NAME)) {
+                // QQ登录
+                Toast.makeText(RegisterActivity.this,"QQ.NAME",Toast.LENGTH_SHORT).show();
+
+                token = platDB.getToken();
+                userId = platDB.getUserId();
+                name = hashMap.get("nickname").toString(); // 名字
+                gender = hashMap.get("gender").toString(); // 年龄
+                headImageUrl = hashMap.get("figureurl_qq_2").toString(); // 头像figureurl_qq_2 中等图，figureurl_qq_1缩略图
+                Toast.makeText(RegisterActivity.this,token+"\n"+userId+"\n"+name+"\n"+gender+"\n"+headImageUrl,Toast.LENGTH_SHORT).show();
+
+            }
+        }
+    }
+
+    @Override
+    public void onError(Platform platform, int i, Throwable throwable) {
+
+    }
+
+    @Override
+    public void onCancel(Platform platform, int i) {
+
+    }
+
+    @Override
+    protected void onDestroy() {
+//        ShareSDK.unregisterPlatform(platDB);//平台注销
+        super.onDestroy();
+    }
 }
